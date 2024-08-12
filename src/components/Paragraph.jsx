8a39241c2f1c64277b4f2cddd1afcfd496603285
NISHAN THAPA <138146815+TNishan10@users.noreply.github.com>
2024-08-12 05:17:08 +0000

    updated

---
+++ src/components/Paragraph.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Paragraph(props) {
+  return (
+    <div>
+      <p className="info">{props.ParagraphInfo}</p>
+    </div>
+  );
+}
+
+export default Paragraph;
